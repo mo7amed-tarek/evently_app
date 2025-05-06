ba3278453bbdb578f6d8cec5612e1cdb4bd53f7c
mohamed <mt917154@gmail.com>
2025-05-06 16:59:52 +0300

    +splash +start + onbording + localization

---
+++ evently_app/lib/core/resoources/assets_manager.dart
@@ -0,0 +1,15 @@
+abstract final class AssetsManager {
+  static const String logo = ("assets/image/Logo.png");
+  static const String brand = ("assets/image/brand.png");
+  static const String titel = ("assets/image/titel.png");
+  static const String start = ("assets/image/start.png");
+  static const String startdark = ("assets/image/start_dark.png");
+  static const String us = ("assets/image/Us.svg");
+  static const String eg = ("assets/image/EG.svg");
+  static const String moon = ("assets/image/Moon.svg");
+  static const String sunLight = ("assets/image/SunLight.svg");
+  static const String sunDark = ("assets/image/SunDark.svg");
+  static const String on1 = ("assets/image/on_1.png");
+  static const String on2 = ("assets/image/on_2.png");
+  static const String on3 = ("assets/image/on_3.png");
+}
