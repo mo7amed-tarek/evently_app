14249d74c7ae5a78f026cdfbd0358c9f9eeea839
mohamed <mt917154@gmail.com>
2026-05-03 15:54:35 +0300

    ++

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -21,8 +21,8 @@ plugins {
 
     id("com.android.application") version "8.7.0" apply false
 
-    // ✅ Kotlin version (REQUIRED for Flutter)
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+   
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 
     // FlutterFire
     id("com.google.gms.google-services") version "4.3.15" apply false
